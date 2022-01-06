e46087c29a36945b6e5a56a87c4cdf06c7cf6c9f
Kevin Johr <kevin.johr@posteo.de>
2022-01-06 20:06:27 +0100

    player switch animation add

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@ let fieldContent = [];
 let currentPlayer = 'cross';
 
 function fillField(id) {
-    if ( currentPlayer == 'cross') {
-        currentPlayer = 'circle';
-    } else {
-        currentPlayer = 'cross';
-    }
 
-    fieldContent[id] = currentPlayer;
-    playerMove();
-    checkIfWin();
+    if (!fieldContent[id]) {
+
+        if ( currentPlayer == 'cross') {
+            currentPlayer = 'circle';
+            document.getElementById('player-1').classList.remove('active-player')
+            document.getElementById('player-2').classList.add('active-player')
+        } else {
+            currentPlayer = 'cross';
+            document.getElementById('player-1').classList.add('active-player')
+            document.getElementById('player-2').classList.remove('active-player')
+        }
+    
+        fieldContent[id] = currentPlayer;
+        playerMove();
+        checkIfWin();
+
+    }
 }
 
 
@@ -33,35 +42,35 @@ function playerMove() {
 function checkIfWin() {
     let winner;
 
-    if (fieldContent[0] == fieldContent[1] && fieldContent[2] == fieldContent[0]) {
+    if (fieldContent[0] == fieldContent[1] && fieldContent[1] == fieldContent[2] && fieldContent[0]) {
         winner = fieldContent[0];
     }
 
-    if (fieldContent[3] == fieldContent[4] && fieldContent[5] == fieldContent[3]) {
+    if (fieldContent[3] == fieldContent[4] && fieldContent[4] == fieldContent[5] && fieldContent[3]) {
         winner = fieldContent[3];
     }
 
-    if (fieldContent[6] == fieldContent[7] && fieldContent[8] == fieldContent[6]) {
+    if (fieldContent[6] == fieldContent[7] && fieldContent[7] == fieldContent[8] && fieldContent[6]) {
         winner = fieldContent[6];
     }
 
-    if (fieldContent[0] == fieldContent[3] && fieldContent[6] == fieldContent[0]) {
+    if (fieldContent[0] == fieldContent[3] && fieldContent[3] == fieldContent[6] && fieldContent[0]) {
         winner = fieldContent[0];
     }
 
-    if (fieldContent[1] == fieldContent[4] && fieldContent[7] == fieldContent[1]) {
+    if (fieldContent[1] == fieldContent[4] && fieldContent[4] == fieldContent[7] && fieldContent[1]) {
         winner = fieldContent[1];
     }
 
-    if (fieldContent[2] == fieldContent[5] && fieldContent[8] == fieldContent[2]) {
+    if (fieldContent[2] == fieldContent[5] && fieldContent[5] == fieldContent[8] && fieldContent[2]) {
+        winner = fieldContent[2];
+    }
+
+    if (fieldContent[0] == fieldContent[4] && fieldContent[4] == fieldContent[8] && fieldContent[0]) {
         winner = fieldContent[0];
     }
 
-    if (fieldContent[0] == fieldContent[4] && fieldContent[8] == fieldContent[0]) {
-        winner = fieldContent[0];
-    }
-
-    if (fieldContent[2] == fieldContent[4] && fieldContent[6] == fieldContent[2]) {
+    if (fieldContent[2] == fieldContent[4] && fieldContent[4] == fieldContent[6] && fieldContent[2]) {
         winner = fieldContent[2];
     }
 
