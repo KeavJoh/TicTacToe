de6af9d541920c128710c6e74758c53517d205c1
Kevin Johr <kevin.johr@posteo.de>
2022-01-06 20:51:59 +0100

    win animation for playground

--- js/main.js
+++ js/main.js
@@ -44,34 +44,58 @@ function checkIfWin() {
 
     if (fieldContent[0] == fieldContent[1] && fieldContent[1] == fieldContent[2] && fieldContent[0]) {
         winner = fieldContent[0];
+        document.getElementById('0').classList.add('win-background');
+        document.getElementById('1').classList.add('win-background');
+        document.getElementById('2').classList.add('win-background');
     }
 
     if (fieldContent[3] == fieldContent[4] && fieldContent[4] == fieldContent[5] && fieldContent[3]) {
         winner = fieldContent[3];
+        document.getElementById('3').classList.add('win-background');
+        document.getElementById('4').classList.add('win-background');
+        document.getElementById('5').classList.add('win-background');
     }
 
     if (fieldContent[6] == fieldContent[7] && fieldContent[7] == fieldContent[8] && fieldContent[6]) {
         winner = fieldContent[6];
+        document.getElementById('6').classList.add('win-background');
+        document.getElementById('7').classList.add('win-background');
+        document.getElementById('8').classList.add('win-background');
     }
 
     if (fieldContent[0] == fieldContent[3] && fieldContent[3] == fieldContent[6] && fieldContent[0]) {
         winner = fieldContent[0];
+        document.getElementById('0').classList.add('win-background');
+        document.getElementById('3').classList.add('win-background');
+        document.getElementById('6').classList.add('win-background');
     }
 
     if (fieldContent[1] == fieldContent[4] && fieldContent[4] == fieldContent[7] && fieldContent[1]) {
         winner = fieldContent[1];
+        document.getElementById('1').classList.add('win-background');
+        document.getElementById('4').classList.add('win-background');
+        document.getElementById('7').classList.add('win-background');
     }
 
     if (fieldContent[2] == fieldContent[5] && fieldContent[5] == fieldContent[8] && fieldContent[2]) {
         winner = fieldContent[2];
+        document.getElementById('2').classList.add('win-background');
+        document.getElementById('5').classList.add('win-background');
+        document.getElementById('8').classList.add('win-background');
     }
 
     if (fieldContent[0] == fieldContent[4] && fieldContent[4] == fieldContent[8] && fieldContent[0]) {
         winner = fieldContent[0];
+        document.getElementById('0').classList.add('win-background');
+        document.getElementById('4').classList.add('win-background');
+        document.getElementById('8').classList.add('win-background');
     }
 
     if (fieldContent[2] == fieldContent[4] && fieldContent[4] == fieldContent[6] && fieldContent[2]) {
         winner = fieldContent[2];
+        document.getElementById('2').classList.add('win-background');
+        document.getElementById('4').classList.add('win-background');
+        document.getElementById('6').classList.add('win-background');
     }
 
     if (winner) {
